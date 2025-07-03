ba7cbe06cc61696809062ecdd1c07206d1cedb4f
Gabriel Caixeta Romero <gabrielcaixetahomero.com>
2025-07-03 15:20:38 -0300

    Add technical indindicators

--- python/generate.py
+++ python/generate.py
@@ -2,7 +2,8 @@ import sys
 import json
 from model import train_model
 from news_model import train_model_news
-from news_scraper import get_news_articles  # você precisa ter esse arquivo
+from news_scraper import get_news_articles
+from technicals import generate_technical_chart  # certifique-se que o arquivo se chame technicals.py
 
 def main():
     if len(sys.argv) < 2:
@@ -15,6 +16,7 @@ def main():
         # Rodar modelos
         today_price, next_price, image_price = train_model(ticker)
         news_prediction, news_image = train_model_news(ticker)
+        technical_image = generate_technical_chart(ticker)
 
         # Coletar notícias com sentimento
         news_articles = get_news_articles(ticker)
@@ -27,10 +29,10 @@ def main():
             "prediction": news_prediction,
             "imagePrice": f"/images/{ticker}_price.png",
             "imageNews": f"/images/{ticker}_news.png",
+            "imageTechnical": f"/images/{ticker}_technical.png",
             "news": news_articles
         }
 
-        # Printar JSON para o route.ts ler
         print(json.dumps(result))
 
     except Exception as e:
